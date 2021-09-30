9da9d2220f3d0ae1f1d8f948bcf9bf5ff54c0fdd
RizwanShaikh22 <rizwanshaikh199091@gmail.com>
2021-09-30 17:15:03 +0530

    new changes

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+
 if (workbox) {
     console.log("Yay! Workbox is loaded !");
     workbox.precaching.precacheAndRoute([]);
